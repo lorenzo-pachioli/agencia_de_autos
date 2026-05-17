dc2a6a88a35ad5aee0ea621845f0583654cf03b1
lorenzo-pachioli <lorenzopachioli@gmail.com>
2026-05-17 19:35:47 -0300

    SCRUM-26: Crear entidad Transaccion

--- src/main/java/utn/programacion3/agencia_de_autos/model/Transaccion.java
+++ src/main/java/utn/programacion3/agencia_de_autos/model/Transaccion.java
@@ -1,4 +1,54 @@
 package utn.programacion3.agencia_de_autos.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+import utn.programacion3.agencia_de_autos.model.enums.EstadoTransaccion;
+import utn.programacion3.agencia_de_autos.model.enums.MetodoPago;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Transaccion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String observaciones;
+
+    @Builder.Default
+    private LocalDate created_at = LocalDate.now();
+
+    private LocalDate updated_at;
+
+    @Column(nullable = false)
+    private BigDecimal comosion_calculada;
+
+    @Column(nullable = false)
+    private BigDecimal precio_final;
+
+    @Enumerated
+    @Column(nullable = false)
+    private MetodoPago metodoPago;
+
+    @Enumerated
+    @Builder.Default
+    private EstadoTransaccion estadoTransaccion = EstadoTransaccion.RESERVA;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "vehiculo_id")
+    private Vehiculo vehiculo;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "cliente_id")
+    private Usuario cliente;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "vendedor_id")
+    private Usuario vendedor;
 }
--- src/main/java/utn/programacion3/agencia_de_autos/model/enums/EstadoTransaccion.java
+++ src/main/java/utn/programacion3/agencia_de_autos/model/enums/EstadoTransaccion.java
@@ -1,4 +1,5 @@
 package utn.programacion3.agencia_de_autos.model.enums;
 
 public enum EstadoTransaccion {
+    RESERVA, SENIADO, VENDIDO, CANCELARO;
 }
