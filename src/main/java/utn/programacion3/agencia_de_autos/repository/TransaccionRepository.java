7e9df29c703b90c110f67d160a64d07299c0912b
lorenzo-pachioli <lorenzopachioli@gmail.com>
2026-05-18 19:01:31 -0300

    SCRUM-32: Crear TransaccionRepository

--- src/main/java/utn/programacion3/agencia_de_autos/repository/TransaccionRepository.java
+++ src/main/java/utn/programacion3/agencia_de_autos/repository/TransaccionRepository.java
@@ -1,4 +1,9 @@
 package utn.programacion3.agencia_de_autos.repository;
 
-public interface TransaccionRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import utn.programacion3.agencia_de_autos.model.Transaccion;
+
+@Repository
+public interface TransaccionRepository extends JpaRepository<Transaccion,Long> {
 }
