08b78b5311d1dad668db69e5ae54560e5b214e5c
lorenzo-pachioli <lorenzopachioli@gmail.com>
2026-05-18 18:15:36 -0300

    SCRUM-29: Crear TransaccionMapper

--- src/main/java/utn/programacion3/agencia_de_autos/mapper/TransaccionMapper.java
+++ src/main/java/utn/programacion3/agencia_de_autos/mapper/TransaccionMapper.java
@@ -1,4 +1,17 @@
 package utn.programacion3.agencia_de_autos.mapper;
 
+import org.mapstruct.Mapping;
+import utn.programacion3.agencia_de_autos.dto.request.TransaccionRequestDTO;
+import utn.programacion3.agencia_de_autos.dto.response.TransaccionResponseDTO;
+import utn.programacion3.agencia_de_autos.model.Transaccion;
+
 public interface TransaccionMapper {
+
+    @Mapping(target = "id", ignore = true)
+    Transaccion toEntity(TransaccionRequestDTO transaccionRequestDto);
+
+    @Mapping(target = "vehiculo_id", source = "vehiculo.id")
+    @Mapping(target = "cliente_id", source = "cliente.id")
+    @Mapping(target = "vendedor_id", source = "vendedor.id")
+    TransaccionResponseDTO toDto(Transaccion transaccion);
 }
