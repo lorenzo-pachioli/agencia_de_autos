4898aaf640d1c16269aad8c071c9cfe5ede135ab
Emiliano Osorio <osorioemiliano43@gmail.com>
2026-05-19 01:51:58 -0300

    AG-10 crea repositories del catalogo
    Se agregan repositories para Marca, Modelo, Vehiculo e ImagenVehiculo

--- src/main/java/utn/programacion3/agencia_de_autos/repository/VehiculoRepository.java
+++ src/main/java/utn/programacion3/agencia_de_autos/repository/VehiculoRepository.java
@@ -1,4 +1,6 @@
 package utn.programacion3.agencia_de_autos.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import utn.programacion3.agencia_de_autos.model.Vehiculo;
 
-public interface VehiculoRepository {
+public interface VehiculoRepository extends JpaRepository<Vehiculo, Long> {
 }
