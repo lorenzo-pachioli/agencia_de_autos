0c09b1b7413985e1f9d39562e57bfc547ba3af61
lorenzo-pachioli <lorenzopachioli@gmail.com>
2026-06-10 19:36:38 -0300

    SCRUM-70 Definir AuditoriaTransaccionResponseDTO y el Mapper

--- src/main/java/utn/programacion3/agencia_de_autos/dto/response/AuditoriaTransaccionResponseDTO.java
+++ src/main/java/utn/programacion3/agencia_de_autos/dto/response/AuditoriaTransaccionResponseDTO.java
@@ -1,45 +1,34 @@
-package utn.programacion3.agencia_de_autos.model;
+package utn.programacion3.agencia_de_autos.dto.response;
 
-import jakarta.persistence.*;
 import lombok.*;
 import utn.programacion3.agencia_de_autos.model.enums.EstadoTransaccion;
 import utn.programacion3.agencia_de_autos.model.enums.MetodoPago;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-@Entity
 @Getter
 @Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class AuditoriaTransaccion {
+public class AuditoriaTransaccionResponseDTO {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
     private Long transaccion_id;
 
-    @Column(nullable = false)
     private Long vendedor_id;
 
-    @Builder.Default
     private LocalDate created_at = LocalDate.now();
 
     private BigDecimal precioFinalAnterior;
     private BigDecimal precioFinalNuevo;
 
-    @Enumerated(EnumType.STRING)
     private MetodoPago metodoPagoAnterior;
 
-    @Enumerated(EnumType.STRING)
     private MetodoPago metodoPagoNuevo;
 
-    @Enumerated(EnumType.STRING)
     private EstadoTransaccion estadoAnterior;
 
-    @Enumerated(EnumType.STRING)
     private EstadoTransaccion estadoNuevo;
 }
