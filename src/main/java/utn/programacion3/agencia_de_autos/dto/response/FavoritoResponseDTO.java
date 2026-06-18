2e4374f399fd69d5db828e1fc38726cedd6b78da
lorenzo-pachioli <lorenzopachioli@gmail.com>
2026-06-18 10:44:20 -0300

    fix: Loop infinito recurrente en Favoritos solucionado

--- src/main/java/utn/programacion3/agencia_de_autos/dto/response/FavoritoResponseDTO.java
+++ src/main/java/utn/programacion3/agencia_de_autos/dto/response/FavoritoResponseDTO.java
@@ -3,7 +3,6 @@ package utn.programacion3.agencia_de_autos.dto.response;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import utn.programacion3.agencia_de_autos.model.Vehiculo;
 
 @Data
 @AllArgsConstructor
@@ -11,6 +10,6 @@ import utn.programacion3.agencia_de_autos.model.Vehiculo;
 public class FavoritoResponseDTO {
 
     private Long id;
-    private Vehiculo vehiculo;
+    private VehiculoResponseDTO vehiculo;
 
 }
