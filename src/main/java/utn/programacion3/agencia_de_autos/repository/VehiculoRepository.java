285cc87c0a0618a4cde248584f99914b0ff8cd42
lorenzo-pachioli <lorenzopachioli@gmail.com>
2026-08-30 17:21:54 -0300

    codigo sin uso borrado

--- src/main/java/utn/programacion3/agencia_de_autos/repository/VehiculoRepository.java
+++ src/main/java/utn/programacion3/agencia_de_autos/repository/VehiculoRepository.java
@@ -1,16 +1,13 @@
 package utn.programacion3.agencia_de_autos.repository;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import utn.programacion3.agencia_de_autos.model.Vehiculo;
 import utn.programacion3.agencia_de_autos.model.enums.EstadoVehiculo;
-import utn.programacion3.agencia_de_autos.model.enums.TipoCombustible;
 
-import java.math.BigDecimal;
-import java.util.List;
 import java.util.Optional;
+
 @Repository
 
 public interface VehiculoRepository extends
